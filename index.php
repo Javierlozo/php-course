c563ca2b4ec93e466f4ee7b0c762077504b84787
Luis Lozoya <luisloart@gmail.com>
2023-07-24 21:45:28 -0400

    Created index.php

---
+++ index.php
@@ -0,0 +1,37 @@
+<!-- GETTING STARTER WITH PHP -->
+<?php
+    echo"Hello World! <br>";
+    echo"Hello!!";
+    // This is a comment
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <br>
+    <button>Order Pizza</button>
+</body>
+</html>
+<br>
+<br>
+
+
+<!-- VARIABLES -->
+<?php
+    $name = "Luis Lozoya";
+    $food = "Pizza";
+    $email = "luis@gmail.com";
+    $age = 21;
+    $users = 2;
+
+    echo "Hello {$name} <br>";
+    echo "You like {$food} <br>";
+    echo "Your email is {$email} <br>";
+    echo "You are {$age} years old <br>";
+    echo "You have {$users} users <br>";
+?>
